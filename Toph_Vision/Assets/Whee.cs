3b78af13a9f8ebefa81fcba3d9cab6e7e259f95f
Gram Nitschke <gramnitschke@gmail.com>
2020-06-04 14:13:24 -0700

    projectiles spawn rings and better arena

---
+++ Toph_Vision/Assets/Whee.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Whee : MonoBehaviour
+{
+    public Material mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        mat.SetFloat("_Offset", Time.time);
+        print("Points colliding: " + other.contacts.Length);
+        print("First normal of the point that collide: " + other.contacts[0].normal);
+        mat.SetVector("_ImpulsePoint", other.contacts[0].point);
+        timer = 5f;
+    }
+
+    float timer = 0f;
+    private void Update()
+    {
+
+        if (timer > 0)
+        {
+            mat.SetInt("_Switch", 1);
+            //mat.SetFloat("_Wavelength", 10 * (5f - timer));
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            mat.SetInt("_Switch", 0);
+        }
+    }
+}
