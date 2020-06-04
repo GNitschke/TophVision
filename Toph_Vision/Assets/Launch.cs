603815e4c8328e653711bb6d69fdabf831e1f2d3
Gram Nitschke <gramnitschke@gmail.com>
2020-06-04 13:35:19 -0700

    projectile

---
+++ Toph_Vision/Assets/Launch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launch : MonoBehaviour
+{
+    public GameObject projectile;
+    public float strength;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            GameObject p = Instantiate(projectile, transform.position, projectile.transform.rotation);
+            p.GetComponent<Rigidbody>().AddForce(transform.forward * strength);
+        }
+    }
+}
